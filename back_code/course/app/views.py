c7bcf76bb0dafee99ab05a0bfe5a5516bbca7784
cmily <1786386686@qq.com>
2020-01-11 20:54:51 +0800

    Xiu gai creatr_match interface Ref #95

--- back_code/course/app/views.py
+++ back_code/course/app/views.py
@@ -274,48 +274,39 @@ def Match_show(request):
     return JsonResponse(data,json_dumps_params={'ensure_ascii':False})
 
 
-#教师创建比赛
+# 教师:创建比赛
 def Create_match(request):
-    name = request.POST.get('match_name')
-    # name = "一站到底"
-    # 比赛总分
-    matchpoint = request.POST.get('match_point')
-    # matchpoint = 100
+    teacher_id = int(request.POST.get('teacher_id'))
+    # teacherid = 2
+    match_name = request.POST.get('match_name')
+    # 比赛积分
+    match_point = int(request.POST.get('match_point'))
+    book_id = int(request.POST.get('book_id'))
+    start_time = request.POST.get('start_time').strftime("%Y-%m-%d %H:%M:%S")
+    end_time = request.POSt.get('end_time').strftime("%Y-%m-%d %H:%M:%S")
+    match_time = request.POST.get('match_time')
 
-    bookname = request.POST.get('book_name')
-    # bookname = "七年级英语"
-    category = Category.objects.get(book_name = bookname)
-    wordids = Classification.objects.filter(
-        category_id = category.category_id).order_by('word_id').values('word_id')
-    print(wordids,type(wordids))
+    teacher = Teacher.objects.get(teacher_id = teacher_id)
+    wordids = Classification.objects.filter(category_id = book_id).order_by('word_id').values('word_id')
+
     words = str()
     for item in wordids:
         words = words + '|' +str(item['word_id'])
     words = words[1:]
-    
-    teacherid = request.POST.get('teacher_id')
-    # teacherid = 2
-    teacher = Teacher.objects.get(teacher_id = teacherid)
-    teachername = teacher.teacher_name
-    starttime = request.POST.get('start_time').strftime("%Y-%m-%d %H:%M:%S")
-    endtime = request.POSt.get('end_time').strftime("%Y-%m-%d %H:%M:%S")
-    matchtime = request.POST.get('match_time').strftime("%Y-%m-%d %H:%M:%S")
-    # starttime = "2020-01-01 15:09:17"
-    # endtime = "2020-01-01 15:09:17"
-    # matchtime = "00:10:00"
 
     m = Match(
-        match_name = name, 
-        match_point = matchpoint,
+        match_name = match_name, 
+        match_point = match_point,
         words_id = words,
-        teacher_id = teacherid,
-        teacher_name = teachername,
-        start_time = starttime,
-        end_time = endtime,
-        match_time = matchtime
+        teacher_id = teacher_id,
+        teacher_name = teacher.teacher_name,
+        start_time = start_time,
+        end_time = end_time,
+        match_time = match_time
     )
     m.save()
-    return JsonResponse(None,safe = False)
+    data = {'status' : 'succeed'}
+    return JsonResponse(data,safe = False)
 
 #学生端获取学生的学习统计
 def Student_statistics(request):
